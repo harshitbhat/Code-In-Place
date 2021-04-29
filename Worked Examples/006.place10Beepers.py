2ac52e76fb74a51d9e4079e62da49c6d0f73231a
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-04-29 16:31:40 +0530

    Place 10 beepers

---
+++ Worked Examples/006.place10Beepers.py
@@ -0,0 +1,14 @@
+from karel.stanfordkarel import *
+
+"""
+File: ForLoopWarmup.py
+------------------------------
+Place 10 beepers.
+"""
+
+def main():
+    for i in range(10):
+        put_beeper()
+
+if __name__ == "__main__":
+    run_karel_program()
