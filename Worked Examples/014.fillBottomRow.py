d6c0cccc091806778bd463d915b3c038d4e87094
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-04-29 16:55:53 +0530

    Fill bottom row

---
+++ Worked Examples/014.fillBottomRow.py
@@ -0,0 +1,16 @@
+from karel.stanfordkarel import *
+
+"""
+File: WhileLoopWarmup.py
+------------------------------
+Fill the entire bottom row of the world with beepers.
+"""
+
+def main():
+    while front_is_clear():
+        put_beeper()
+        move()
+    put_beeper()
+
+if __name__ == "__main__":
+    run_karel_program()
