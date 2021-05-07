b1e28572d464bdc0134afe6d27bec3ad48f07a53
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-05-08 01:41:49 +0530

    Remainder division

---
+++ Worked Examples/039.division.py
@@ -0,0 +1,7 @@
+def main():
+    d = int(input('Please enter an integer to be divided: '))
+    q = int(input('Please enter an integer to divide by: '))
+    print(f'The result of this division is {d // q} with a remainder of {d  % q}')
+
+if __name__ == "__main__":
+    main()
