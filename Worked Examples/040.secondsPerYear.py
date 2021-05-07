5bfa5e0d13404d20d8f51f7469826192790fd466
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-05-08 01:44:09 +0530

    Seconds in a year

---
+++ Worked Examples/040.secondsPerYear.py
@@ -0,0 +1,9 @@
+def main():
+    DAYS = 365
+    HOURS = 24
+    MINUTES = 60
+    SECONDS = 60
+    print(f'The number of seconds in a year = {DAYS * HOURS * MINUTES * SECONDS}')
+
+if __name__ == "__main__":
+    main()
