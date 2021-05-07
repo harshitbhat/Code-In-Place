2d2e13c93245040a5d4cdda917945bd423cc907d
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-05-08 01:37:43 +0530

    Perimeter of a triangle

---
+++ Worked Examples/037.perimeter.py
@@ -0,0 +1,8 @@
+def main():
+    a = float(input('What is the length of side 1? '))
+    b = float(input('What is the length of side 2? '))
+    c = float(input('What is the length of side 3? '))
+    print(f'The perimeter of the triangle is {a + b + c}')
+
+if __name__ == "__main__":
+    main()
