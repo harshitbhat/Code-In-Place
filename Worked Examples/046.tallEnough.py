ab5707dcca9973847f1d9a556967999f5f994375
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-05-08 01:58:09 +0530

    Tall enough to ride

---
+++ Worked Examples/046.tallEnough.py
@@ -0,0 +1,9 @@
+def main():
+    height = int(input('How tall are you? '))
+    if height > 50:
+        print('You\'re tall enough to ride!')
+    else:
+        print('You\'re not tall enough to ride!')
+
+if __name__ == "__main__":
+    main()
