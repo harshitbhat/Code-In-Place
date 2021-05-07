f5e206e88bd1851f7cb62556b54d704d7b0f6f34
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-05-08 01:46:11 +0530

    Tiny mad libs

---
+++ Worked Examples/041.tinyMadLibs.py
@@ -0,0 +1,10 @@
+SENTENCE_START = "Code in Place is fun. I learned to program and used Python to make my " # adjective noun verb
+
+def main():
+    ad = input('Please type an adjective and press enter. ')
+    n = input('Please type an noun and press enter. ')
+    v = input('Please type an verb and press enter. ')
+    print(SENTENCE_START)
+
+if __name__ == "__main__":
+    main()
