3ad7162311aec497f2c44fc04091cf20cd126d37
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-05-08 01:32:10 +0530

    How old are they?

---
+++ Worked Examples/034.ages.py
@@ -0,0 +1,15 @@
+def main():
+    anton = 21
+    beth = 21 + 6
+    chen = beth + 20
+    drew = chen + anton
+    ethan = chen
+
+    print('Anton: ',anton)
+    print('Beth: ',beth)
+    print('Chen: ',chen)
+    print('Drew: ',drew)
+    print('Ethan: ',ethan)
+
+if __name__ == '__main__':
+    main()
